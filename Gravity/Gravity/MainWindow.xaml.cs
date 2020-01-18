f1a8e74b24ce3b487ccfe693475922c870919f1c
Przemysław Janusz Borek <braciab89@gmail.com>
2020-01-18 01:13:01 +0100

    Usunięcie wywołania usuniętych okien

--- Gravity/Gravity/MainWindow.xaml.cs
+++ Gravity/Gravity/MainWindow.xaml.cs
@@ -12,23 +12,5 @@ namespace Gravity
         {
             InitializeComponent();
         }
-
-        private void btnPendulum_Click(object sender, RoutedEventArgs e)
-        {
-            PendulumWindow pendulumWindow = new PendulumWindow();
-            pendulumWindow.Show();
-        }
-
-        private void btnAgeAndWeight_Click(object sender, RoutedEventArgs e)
-        {
-            AgeAndWeightWindow ageAndWeightWindow = new AgeAndWeightWindow();
-            ageAndWeightWindow.Show();
-        }
     }
-    /*
-    *  Przerzucić zasoby do app (np lista użytkowników)
-    * zrobić log dla każdego użytkownika
-    * zrobić resultswindow zamiast messageboxów
-    * poprawić czytelność 
-    */
 }
